bcc4cb153f54e343855dc429931c55f0cf916db5
OrelS0rek <orelsorekml@gmail.com>
2026-02-09 11:41:24 +0200

    Initial Commit

---
+++ GhostRunner/GhostRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostRunnerApp.swift
+//  GhostRunner
+//
+//  Created by Admin on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GhostRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
